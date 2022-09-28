f17a0879c2b3f2d1bba4d2052b2d1221f72e1026
macbookair <medaminetanouyat4@gmail.com>
2022-09-28 03:40:15 +0100

    put and delete product

--- server/services/product.js
+++ server/services/product.js
@@ -29,3 +29,22 @@ export const createProduct = async(req, res) => {
     });
 }
 
+//handle update product request
+export const updateProduct = async(req, res) => {
+    //store request body in object
+    const {_id, label, category, available, quantity, description} = req.body;
+    //updating by using findOneAndUpdate witch takes the match object and the update object as arguments
+    const product = await Product.findOneAndUpdate({_id: _id},
+        { label, category, available, quantity, description}, 
+        { new: true}).catch((err) => res.json(err));
+    res.json(product);
+}
+
+//handle delete product request
+export const deleteProduct = async(req, res) => {
+    //retrieve product id from request body
+    const id = req.body._id;
+    const product = await Product.findByIdAndDelete(id).catch( (err) => res.json(err));
+    res.json(product);
+}
+
