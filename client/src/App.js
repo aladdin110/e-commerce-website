9213768943d19ebfecf2a4f59ba977a7f4420e0b
ambealla <benallaamin22@gmail.com>
2022-11-10 21:04:31 +0100

    add folders

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,18 @@ import React, { useRef, lazy, Suspense, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./Component/Navbar/navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AboutUs from "./Component/aboutus/AboutUs";
-import Shop from "./Component/Shop/Shop";
-import Home from "./Component/home/Home";
+import { ToastProvider } from "react-toast-notifications";
+import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
+
+/* 
+ */
+const Home = lazy(() => import("./Component/home/Home"));
+const Shop = lazy(() => import("./Component/Shop/Shop"));
+const AboutUs = lazy(() => import("./Component/aboutus/AboutUs"));
+/* 
+ */
+
+
 
 function App() {
   const dispatch = useDispatch();
@@ -13,18 +22,36 @@ function App() {
   console.log(Product);
 
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="shop" element={<Shop />}></Route>
-          <Route path="aboutus" element={<AboutUs />}></Route>
-          <Route replace to="/" />
-        </Routes>
-      </BrowserRouter>
-      
-    </div>
+    <ToastProvider placement="bottom-left">
+  
+      <div className="App">
+   
+        <BreadcrumbsProvider>
+          <BrowserRouter>
+          <>
+            <Suspense
+              fallback={
+                <div className="flone-preloader-wrapper">
+                  <div className="flone-preloader">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+              }
+            >
+              <Navbar />
+              <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+                <Route path="shop" element={<Shop />}></Route>
+                <Route path="aboutus" element={<AboutUs />}></Route>
+                <Route replace to="/" />
+              </Routes>
+            </Suspense>
+            </>
+          </BrowserRouter>
+        </BreadcrumbsProvider>
+      </div>
+    </ToastProvider>
   );
 }
 
